e5a7154d8958ba23ba51815bfccf6be8ecba05ed
Dan Belden <dan@free-fall.co.uk>
2017-12-20 07:56:43 +0000

    [PHP] Adding new docker files for PHP 7.2 with FPM & NGINX

---
+++ ubuntu-php72-fpm-nginx/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+# Append ENV vars to the FPM config if not in ignore set and value is not empty
+# https://github.com/docker-library/php/issues/74
+# https://ypereirareis.github.io/blog/2015/12/13/symfony-nginx-environment-variables-php-fpm/
+ENV_VARS=`printenv`
+IGNORE_ENV_VARS=( "_" "DEBIAN_FRONTEND" "HOME" "HOSTNAME" "LS_COLORS" "no_proxy" "PATH" "PWD" "SHLVL" "TERM" )
+for ENV_VAR in $ENV_VARS
+do
+  IFS== read ENV_VAR_NAME ENV_VAR_VALUE <<< "$ENV_VAR"
+  if [[ ! ${IGNORE_ENV_VARS[*]} =~ "$ENV_VAR_NAME" ]] && [[ !  -z  $ENV_VAR_VALUE ]]; then
+    echo "env[$ENV_VAR_NAME] = $ENV_VAR_VALUE" >> /usr/local/etc/php/fpm/pool.d/www.conf
+  fi
+done
+
+# Run the web services
+service php-fpm start
+nginx
