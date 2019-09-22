cb9c794f18c96531a10545686beccdd8fe42693e
Dan Belden <dan@free-fall.co.uk>
2019-09-22 16:01:35 +0100

    [CircleCI] Switch image building to CircleCI for speed

---
+++ tools/ci-build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Check the folder name on arg1 is set
+if [[ -z $1 ]]; then
+  echo "[ERROR] No build folder provided in arg1!"
+  exit 1
+fi
+
+# Check the path exists
+BUILD_PATH="$1"
+if [[ ! -d "$BUILD_PATH" ]]; then
+  echo "[ERROR] \"$BUILD_PATH\" is not a valid build path!"
+  exit 1
+fi
+
+# [Hack] Realpath function hack for OSX
+# https://stackoverflow.com/questions/3572030/bash-script-absolute-path-with-os-x
+realpath() {
+  [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+# Create the remaining vars for docker build
+BUILD_PATH_FULL="$(realpath $BUILD_PATH)"
+BUILD_PATH_LAST_DIR="$(echo $BUILD_PATH_FULL | rev | cut -d'/' -f1 | rev)"
+IMAGE_TAG="danbelden/${BUILD_PATH_LAST_DIR}:ci-build"
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Output the vars for debugging
+echo "--------------- DEBUG ----------------------"
+echo "BUILD_PATH: $BUILD_PATH"
+echo "BUILD_PATH_FULL: $BUILD_PATH_FULL"
+echo "BUILD_PATH_LAST_DIR: $BUILD_PATH_LAST_DIR"
+echo "IMAGE_TAG: $IMAGE_TAG"
+echo "SCRIPT_PATH: $SCRIPT_PATH"
+
+# Launch the docker build process
+$SCRIPT_PATH/docker-build.sh "$BUILD_PATH" "$IMAGE_TAG"
